2466e5a3c7145d69b8bbe8cf807e29de51420852
Victor Chhun <109203183+AwesomlyC@users.noreply.github.com>
2024-10-23 00:48:53 -0700

    added components to SearchPage

--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState }  from 'react'
 import { useLocation } from 'react-router-dom'
 import axios from 'axios';
+import NovelResults from './NovelResults';
+import ViewToggle from './ViewToggle';
 function SearchPage() {
 
     const location = useLocation();
     const [searchQuery, setSearchQuery] = useState("");
     const [searchResults, setSearchResults] = useState([]);
+    const [viewIndex, setViewIndex] = useState(0);
+    
     useEffect(() => {
         // Retrieve all params from the URL such as q=apple
         const params = new URLSearchParams(location.search);
@@ -22,30 +26,8 @@ function SearchPage() {
     return (
         <div>
             <p style={{fontSize: '36px', textAlign: 'center'}}>Search for: <b><em>{searchQuery}</em></b></p> 
-            <div className="light-novels">
-                <ul>
-                    {searchResults.map(novel => (
-                        <li key={novel.mal_id}>
-                            <div className="novel">
-                                <img src={novel.images.jpg.image_url} alt="picture_holder" />
-                                <div className='description'>
-                                    <h3 className="title">
-                                        <a href={`http://localhost:3000/single-ln?id=${novel.mal_id}`}>{novel.title}</a>
-                                    </h3>
-                                    <h5 className="title"><a href={`${novel.url}`}>Link to MyAnimeList's Page</a></h5>
-
-                                    <div className="novel-synopsis">
-                                        <h2><em>Synopsis:</em></h2> {novel.synopsis ? novel.synopsis : "No synopsis information has been found..."}
-                                    </div>
-                                    <div className='genres'>
-                                        <b>Genres: </b>{novel.genres.length > 0 ? novel.genres.map(item => item.name).join(', ') : "N/A"}
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <NovelResults results={searchResults} viewIndex={viewIndex} />
+            <ViewToggle setViewIndex={setViewIndex} />
         </div>
     )
 }
